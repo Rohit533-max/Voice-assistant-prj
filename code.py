6b2731c5f877a4708bf92c81b0c0bcf9a65dcbd8
Rohit533-max <rohit03383@gmail.com>
2025-11-17 21:42:09 +0530

    Base program for the password generator

--- code.py
+++ code.py
@@ -1,2 +1,21 @@
-print("This is new generation")
-print("New starting")
+import string
+import random  #to choose the random elements form the list s
+
+
+if __name__ == "__main__":
+    s1 = string.ascii_letters   
+    #ascii letters contain both upper and lowercase letters
+     
+    s2 = string.digits
+    
+    s3 = string.punctuation  
+    #punctuation =  special char eg:-> @,#,$
+     
+    passlen = int(input("Enter the password length\n"))
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    
+    random.shuffle(s) # to shuffle the list s
+    print("".join (s[0:passlen]))
